b3a527882871b2313ac0c6c82691e89202f34d40
DianaRodionova <dianka225.df@gmail.com>
2023-10-02 20:05:19 +0700

    first commit

---
+++ source/js/script.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".swiper", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+const userList = document.querySelector('.user-list');
+
+navMain.classList.remove('main-nav--nojs');
+userList.classList.remove('user-list--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
